8404bb55a4b0f579bd310de7484ab611352d70cb
Anders Weijnitz <anders.weijnitz@gmail.com>
2014-08-02 16:47:13 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#File Upload With Express 4 and Connect-Busboy
+A basic example of how to use Express 4 routes and Connect-Busboy to handle multiple file uploads.
+
---
+++ routes/upload.js
@@ -0,0 +1,43 @@
+var fs = require('fs');
+var os = require('os');
+
+var handleFile = function(filename, file, targetDir) {
+    var targetFile = targetDir + filename;
+    var fstream;
+
+    console.log("Uploading: " + filename + ' to ' + targetFile);
+    fstream = fs.createWriteStream(targetFile);
+    file.pipe(fstream);
+
+    fstream.on('close', function () {
+        // Note to self: Fire off an event to handle the file in tmp here (check it, thumbs gen, record it, remove it)
+        console.log('Saved file: '+filename);
+    });
+    fstream.on('error', function () {
+        console.log('ERROR while saving file: '+filename);
+    });
+};
+
+
+// Parse form and handle files and fields.
+var handleForm = function(req, res) {
+    var result = { files: [], fields: [] };
+
+    req.busboy.on('file', function (fieldname, file, filename) {
+        result.files.push({ name: filename});
+        handleFile(filename, file, os.tmpdir());
+    });
+    req.busboy.on('field', function(key, value, keyTruncated, valueTruncated) {
+        console.log('Field received: '+key+' = '+value);
+        result.fields.push({ 'name': key, val: value });
+    });
+
+    req.busboy.on('finish', function() {
+        res.render('afterUpload', result);
+    });
+    req.pipe(req.busboy);
+
+};
+
+
+module.exports = handleForm;
